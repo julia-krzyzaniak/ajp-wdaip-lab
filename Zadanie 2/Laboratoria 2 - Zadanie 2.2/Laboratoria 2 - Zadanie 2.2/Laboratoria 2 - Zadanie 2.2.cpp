f3488590ab47cfce6602ae421918235073d2fd41
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-20 20:55:04 +0200

    Zadanie 2.2

---
+++ Zadanie 2/Laboratoria 2 - Zadanie 2.2/Laboratoria 2 - Zadanie 2.2/Laboratoria 2 - Zadanie 2.2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, w;
+    
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+
+    w = a * b;
+
+    if (w > 100) {
+        cout << "Wynik jest wiekszy od 100";
+    }
+
+    else if (w == 100) {
+        cout << "Wynik jest rowny 100";
+    }
+
+    else if (w < 100) {
+        cout << "Wynik jest mniejszy niz 100";
+    }
+}
+
