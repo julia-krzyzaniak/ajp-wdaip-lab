cd385ff5c070983c947a0bc643959e98a74bac32
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-20 20:55:32 +0200

    Zadanie 2.3

---
+++ Zadanie 2/Laboratoria 2 - Zadanie 2.3/Laboratoria 2 - Zadanie 2.3/Laboratoria 2 - Zadanie 2.3.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    float a, b;
+
+    int f;
+
+    cout << "Wybierz dzialanie \n 1. Dodawanie \n 2. Odejmowanie \n 3. Mnozenie \n 4. Dzielenie"<<endl;
+    cout << "Wybierz opcje: ";
+    cin >> f;
+
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+
+    switch (f)
+    {
+    case 1:
+        cout << "Suma to: " << a + b;
+        break;
+
+    case 2:
+        cout << "Roznica to: " << a - b;
+        break;
+
+    case 3:
+        cout << "Iloczyn to: " << a * b;
+        break;
+
+    case 4:
+        if (a == 0 || a == b) {
+            cout << "Nie dziel przez zero!";
+            break;
+        }
+        cout << "Iloraz to: " << a / b;
+        break;
+
+    default:
+        cout << "Blad";
+        break;
+    }
+}
+
+
