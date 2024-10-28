9ee6a0a4327de2c44acdc4967fcf73aa8cca9608
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 21:26:43 +0100

    Zadanie 3.1

---
+++ Zadanie 3/Zadanie 3.1/Zadanie 3.2/Zadanie 3.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, ujemne = 0; 
+
+	do {
+		cout << "Podaj liczbe: ";
+		cin >> a;
+		if (a < 0) ujemne++;
+	} 
+	while (a != 0);
+
+	{
+		cout << "Koniec!" << endl;
+	} 
+
+	cout << "Ilosc liczb ujemnych: " << ujemne;
+}
+
