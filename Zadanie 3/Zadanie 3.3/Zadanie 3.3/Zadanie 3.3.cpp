6ee47f7003d86ac289bbe12774190b86e8432a6c
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 21:27:26 +0100

    Zadanie 3.3

---
+++ Zadanie 3/Zadanie 3.3/Zadanie 3.3/Zadanie 3.3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	int a, b, wynik, lwp = 0, po = 0;
+	char odpowiedz;
+
+	srand(time(NULL));
+
+	do {
+		a = rand() % 11;
+		cout << endl << "Pierwsza liczba: "<< a << endl;
+
+		b = rand() % 11;
+		cout << "Druga Liczba: " << b << endl;
+
+		cout << "Oblicz wynik: ";
+		cin >> wynik;
+
+		if (a * b == wynik) {
+			
+			cout << endl << "Poprawnie" << endl;
+			po = po++;
+		}
+	
+		else
+			cout << endl << "Niepoprawnie" << endl;
+
+		lwp = lwp++;
+
+
+		cout << endl<< "Czy chcesz kontynuowac? Wcisnij t/n : ";
+		cin >> odpowiedz;
+	} 
+	while (odpowiedz == 't');
+
+
+	cout << endl << "Liczba wykonanych prob: " <<lwp<<endl;
+	cout << "Poprawne odpowiedzi: "<< po << endl;
+
+}
+
