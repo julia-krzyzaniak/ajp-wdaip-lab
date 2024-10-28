0cd582c946a28439d86e3f2a254aede482d3443f
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 23:20:44 +0100

    Zadanie 4.3 - inicjalizacja zmiennych

---
+++ Zadanie 4/Zadanie 4.3/Zadanie 4.3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	float rachunek = 0, float wplata = 0;
+
+	// float NOMINALY = { 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 };
+
+	cout << "Podaj kwote rachunku: " << endl;
+	cin >> rachunek;
+
+	cout << "Podaj kwote wplaty: " << endl;
+	cin >> wplata;
+
+	return 0;
+}
+
