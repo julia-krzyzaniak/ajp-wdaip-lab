78553f069ebaa1945797fe37850ff9c943f1fac2
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 21:18:12 +0100

    Zadanie 1.1

---
+++ Zadanie 1/Zadanie 1.1/Laboratoria - Zadanie 1.1/Laboratoria - Zadanie 1.1.cpp
@@ -0,0 +1,20 @@
+﻿#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <climits>
+#include <conio.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	float a, b;
+	
+	cout << "Podaj pierwsza liczbe: ";
+	cin >> a;
+	cout << "Podaj druga liczbe: ";
+	cin >> b;
+	cout << "Pole prostokata: " << a * b << endl;
+	cout << "Obwod prostokata: " << 2 * a + 2 * b << endl;	
+}
