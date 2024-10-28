1e4a1183b6459c8b5a4d18f9bf5aae890291690f
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 21:27:06 +0100

    Zadanie 3.2

---
+++ Zadanie 3/Zadanie 3.2/Zadanie 3.1/Zadanie 3.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, suma = 0;
+
+    do {  
+        cout << "Podaj liczbe: ";
+        cin >> a;
+        suma = suma + a;
+    } 
+    while (suma <= 100);
+    
+    cout << "Suma: "<<suma;
+   
+}
+
+
