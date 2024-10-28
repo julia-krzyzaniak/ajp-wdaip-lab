277e2b86c3f13139c9b53553808de8152721b661
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 21:23:36 +0100

    Zadanie 2.1 if

---
+++ Zadanie 2/Zadanie 2.1 if/Laboratoria 2 - Zadanie 2.1 if/Laboratoria 2 - Zadanie 2.1 if.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int dzien;
+
+    cout << "Wpisz liczbe w zakresie 1-7: ";
+    cin >> dzien;
+
+    if (dzien == 1) {
+        cout << "Poniedzialek";
+    }
+ 
+    else if (dzien == 2) {
+            cout << "Wtorek";
+    }
+
+    else if (dzien == 3) {
+        cout << "Sroda";
+    }
+
+    else if (dzien == 4) {
+        cout << "Czwartek";
+    }
+
+    else if (dzien == 5) {
+        cout << "Piatek";
+    }
+
+    else if (dzien == 6) {
+        cout << "Sobota";
+    }
+
+    else if (dzien == 7) {
+        cout << "Niedziela";
+    }
+
+    else {
+        cout << "Blad";
+    }
+
+   
+
+}
+
