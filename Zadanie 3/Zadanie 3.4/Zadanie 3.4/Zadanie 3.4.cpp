38ed9c0d7488d4b1277a51275438798608510632
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 21:27:48 +0100

    Zadanie 3.4

---
+++ Zadanie 3/Zadanie 3.4/Zadanie 3.4/Zadanie 3.4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, min, max;
+
+ 
+	cout << "Podaj liczbe: ";
+	cin >> a;
+
+	min = a;
+	max = a;
+	
+	while (a != 0)
+		{
+			if (a > max) 
+				max = a;
+
+			if (a < min)
+				min = a;
+
+			cout << "Podaj liczbe: ";
+			cin >> a;
+		 }
+
+	{
+		cout << endl << "Koniec!" << endl;
+		cout << endl;
+		cout << "Najmniejsza liczba: " << min << endl;
+		cout << "Najwieksza liczba: " << max << endl;
+	}
+}
