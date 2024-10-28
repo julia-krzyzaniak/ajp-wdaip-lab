a5d4528cdb2614a86735cf325f7e6cbc3fcf6504
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 21:19:16 +0100

    Zadanie 1.3

---
+++ Zadanie 1/Zadanie 1.3/Lobaratoria 1 - Zadanie 1.3/Lobaratoria 1 - Zadanie 1.3.cpp
@@ -0,0 +1,23 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+    float a, b, w;
+
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+
+    w = pow(a, 3) - 0.5 * pow(b, 4);
+
+    cout << "Wynik = " << w << endl;;
+
+}
+
