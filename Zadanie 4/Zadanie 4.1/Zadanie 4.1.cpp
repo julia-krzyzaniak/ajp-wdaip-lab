971bac5e3b5fe2e5e3b792900e527c19e1a75f9f
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 22:02:26 +0100

    Zadanie 4.1

---
+++ Zadanie 4/Zadanie 4.1/Zadanie 4.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int liczba = 0, suma = 0, w = 0;
+
+	cout << "Podaj liczbe: ";
+	cin >> liczba;
+
+	while (suma < 1000) 
+	{
+		suma = liczba * w;
+		w++;
+		cout << w << " Wielokrtonosc liczby "<< liczba << " to:" << suma << endl;
+	}
+}
