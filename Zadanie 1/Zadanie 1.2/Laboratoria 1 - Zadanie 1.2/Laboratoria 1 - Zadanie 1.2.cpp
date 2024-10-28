a9028a50c67a57562fb3062bf77f6afe4fbf95e4
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-28 21:18:51 +0100

    Zadanie 1.2

---
+++ Zadanie 1/Zadanie 1.2/Laboratoria 1 - Zadanie 1.2/Laboratoria 1 - Zadanie 1.2.cpp
@@ -0,0 +1,20 @@
+﻿#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <climits>
+#include <conio.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	float r;
+	
+	cout << "Podaj promien: ";
+	cin >> r;
+	cout << "Pole kola: " << M_PI * pow(r, 2) << endl;
+	cout << "Obwod kola: " << 2 * M_PI * r << endl;
+		
+	}
+
