8f53b28d60972c89004ccf8d30c7303b22e019f3
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-10-31 00:16:25 +0100

    Zadanie 4.3 - petla while

--- Zadanie 4/Zadanie 4.3/Zadanie 4.3.cpp
+++ Zadanie 4/Zadanie 4.3/Zadanie 4.3.cpp
@@ -5,15 +5,31 @@ using namespace std;
 int main()
 
 {
-	float rachunek = 0, float wplata = 0;
+	int p, i = 0;
+	double rachunek = 0, wplata = 0, reszta = 0;
+	double nominal[] = { 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 };
 
-	// float NOMINALY = { 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 };
 
-	cout << "Podaj kwote rachunku: " << endl;
+	cout << "Podaj kwote rachunku: ";
 	cin >> rachunek;
 
-	cout << "Podaj kwote wplaty: " << endl;
+	cout << "Podaj kwote wplaty: ";
 	cin >> wplata;
+
+	reszta = wplata - rachunek;
+
+	while (reszta > 0.01 ) {
+		
+		if (reszta >= nominal[i]) {
+
+			p = reszta / nominal[i];
+
+			reszta = reszta - (nominal[i] * p);
+			cout << "Reszta: " << nominal[i] << " zl x " << p << endl;
+		}
+		i++;
+		
+	}
 
 	return 0;
 }
