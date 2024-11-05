5ce97aafeb195700c86cf774837e5b664edd3226
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-05 21:33:20 +0100

    Zadanie 5.4

---
+++ Zadanie 5/Zadanie 5.4/Zadanie 5.4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int n, j;
+
+    cout << "Podaj liczbe: ";
+    cin >> n;
+    cout << endl;
+
+    j = n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " " << j << endl;
+        j--;
+    }
+
+}
+
