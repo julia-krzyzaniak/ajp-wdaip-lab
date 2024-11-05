6241fd09296336d90539eaea351a819a3d4a481b
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-05 21:02:24 +0100

    Zadanie 5.2

---
+++ Zadanie 5/Zadanie 5.2/Zadanie 5.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, silnia = 1;
+
+	cout << "Podaj n: ";
+	cin >> n;
+	cout << endl;
+
+	for (int i = 1; i <= n; i++)
+	{
+		silnia = silnia * i;
+		cout << "!" << i << " = " << silnia << endl;
+	}
+
+}
