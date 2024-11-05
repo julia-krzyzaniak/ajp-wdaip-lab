dd388bf1b8bcc0f172ed5f5cc0dd99eeb433cf7a
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-05 20:38:14 +0100

    Zadanie 5.1

---
+++ Zadanie 5/Zadanie 5.1/Zadanie 5.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int ocena;
+	float srednia = 0;
+
+	
+	for (int i = 0; i < 7; i++) 
+	{
+		cout << "Podaj ocene: ";
+		cin >> ocena;
+
+		srednia = srednia + ocena;
+	}
+	
+	srednia = srednia / 7;
+
+	cout << "Srednia z ocen to: " << srednia << endl;
+
+	return 0;
+	
+}
+
