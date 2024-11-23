10c4dd74d3d25e2fcad853445f9c0cb140492353
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-23 17:40:33 +0100

    Zadanie 6.2

---
+++ Zadanie 6/Zadanie 6.2/Zadanie 6.2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int tab[8];
+	int i, suma;
+	float srednia = 0;
+
+	cout << "Program obliczajacy sume i srednie 8 elementow tablicy jednowymiarowej.\n";
+
+	for (i = 0; i < 8; i++)
+	{
+		cout << "Podaj liczbe: ";
+		cin >> tab[i];
+	}
+
+	for (i = 0; i < 8; i++)
+	{
+		cout << tab[i] << " ";
+	}
+
+	for (i = 0, suma = 0; i < 8; i++)
+
+		suma = suma + tab[i];
+		srednia = suma / 8;
+
+	cout << endl << endl << "Suma wynosi: " << suma << "\n";
+	cout <<"Srednia wynosi: " << srednia << "\n";
+}
