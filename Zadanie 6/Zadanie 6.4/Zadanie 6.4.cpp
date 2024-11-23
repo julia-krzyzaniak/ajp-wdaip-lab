c845ff1367267161952ae7305cc36008519ae73c
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-23 22:10:12 +0100

    Zadanie 6.4

---
+++ Zadanie 6/Zadanie 6.4/Zadanie 6.4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int n = 10;
+	int tab1[n] = { 13, 45, 111, 67, 4, 51, 16, 73, 88, 9 }; 
+	int tab2[n] = { 15, 111, 26, 3, 14, 15, 86, 17, 8, 19 };
+	int tabsum[n];
+
+
+	for (int i = 0; i < n; i++)
+	{
+		tabsum[i] = tab1[i] + tab2[i];
+		cout << tab1[i] << " + " << tab2[i] << " = " << tabsum[i] << endl;
+	}
+}
