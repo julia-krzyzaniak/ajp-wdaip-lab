16eed96c636d4f82720562d890b6bd365504319e
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-23 10:23:40 +0100

    Zadanie 6.1

---
+++ Zadanie 6/Zadanie 6.1/Zadanie 6.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int tab[8];
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		cout << "Podaj liczbe: ";
+		cin >> tab[i];	
+	}
+
+	for (i = 0; i < 8; i++)
+	{
+		cout << tab[i] << " ";
+	}
+	
+}
