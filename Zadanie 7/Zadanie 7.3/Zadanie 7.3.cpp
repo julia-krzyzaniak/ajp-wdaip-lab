4fcf1ba7fcf24d27801fae6e45d400d4e6f33a38
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-29 22:34:03 +0100

    Zadanie 7.3

---
+++ Zadanie 7/Zadanie 7.3/Zadanie 7.3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	const int n = 30;
+	int tab1[n];
+	int a;
+
+	cout << "Podaj liczbe: ";
+	cin >> a;
+
+	cout << endl;
+
+	cout << "Program generujacy tablice 30 elementowa o wartosciach losowych z przedzialu od -10 do 10: \n";
+
+	for (int i = 0; i < n; i++)
+	{
+		tab1[i] = (rand() % 21) - 10;
+		cout << tab1[i] << " ";
+	}
+	cout << endl;
+	cout << endl;
+
+	for (int i = 0; i < n; i++)
+
+		if (tab1[i] == a) {
+
+			cout << i << ": wystepuje podana wartosc w tablicy." << endl;
+		}
+
+		else
+			cout << i << ": brak podanej wartosci w tablicy." <<endl;
+
+}
