ea5ea4e60d4e3df4a3a220048b50fe1c101307da
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-29 20:53:08 +0100

    Zadanie 7.1

---
+++ Zadanie 7/Zadanie 7.1/Zadanie 7.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	const int n = 30;
+	int tab1[n];
+
+
+	cout << "Program generujacy tablice 30 elementowa o wartosciach losowych z przedzialu od -10 do 10: \n";
+
+	for (int i = 0; i < n; i++)
+	{
+		tab1[i] = (rand() % 21) - 10;
+		cout << tab1[i] << " " << endl;
+	}
+
+}
