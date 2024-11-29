b0b23779b7be23e1462574b030ab30f5aad53a59
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-29 23:12:14 +0100

    Zadanie 7.5

---
+++ Zadanie 7/Zadanie 7.5/Zadanie 7.5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	const int n = 30;
+	int tab1[n];
+	int a, c = 0;
+
+	cout << "Podaj liczbe: ";
+	cin >> a;
+
+	cout << endl;
+
+	cout << "Program generujacy tablice 30 elementowa o wartosciach losowych z przedzialu od -10 do 10: \n";
+
+	for (int i = 0; i < n; i++)
+	{
+		tab1[i] = (rand() % 21) - 10;
+		cout << tab1[i] << " ";
+	}
+	cout << endl;
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
+		if (tab1[i] == a) {
+
+			c++;
+		}
+
+	}
+	cout << "Ilosc wystapien podanej liczby: " << c;
+
+}
