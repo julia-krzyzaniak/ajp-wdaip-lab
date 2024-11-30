4264a09ed3c773ea18e9f8410acde05ae0c07969
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-30 16:17:24 +0100

    Zadanie 9.2

---
+++ Zadanie 9/Zadanie 9.2/Zadanie 9.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int dodawanie(int a, int b) {
+    int suma;
+    suma = a + b;
+    return suma;
+}
+
+int main()
+{
+    int c, d;
+    
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> c;
+    
+    cout << "Podaj druga liczbe liczbe: ";
+    cin >> d;
+
+    cout << endl << "Wynik dodawania to: " << dodawanie(c, d);
+
+}
