6ff3c47e6c59e4d3b8b0b5f5b558caa871d88629
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-30 16:23:16 +0100

    Zadanie 9.3

---
+++ Zadanie 9/Zadanie 9.3/Zadanie 9.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int max(int a, int b) {
+    
+    if (a > b)
+        return a;
+    
+    return b;
+}
+
+int main()
+{
+    int c, d;
+
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> c;
+
+    cout << "Podaj druga liczbe liczbe: ";
+    cin >> d;
+
+    cout << endl << "Wieksza liczba jest: " << max(c, d);
+}
