add40aa3c95d6feede7e5522301b97eab8bb7306
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-30 17:08:51 +0100

    Zadanie 9.4

---
+++ Zadanie 9/Zadanie 9.4/Zadanie 9.4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <conio.h>
+
+using namespace std;
+
+void wzor(int l, char z) {
+
+    for (int i = 0; i < l; i++) {
+        cout << z;
+    }
+  
+}
+
+int main()
+{
+    int liczba = 1;
+    char znak = 'a';
+    cout << "Podaj liczbe: ";
+    cin >> liczba;
+
+    cout << "Podaj znak: ";
+    cin >> znak;
+
+    cout << endl << endl;
+
+    wzor(liczba, znak);
+}
