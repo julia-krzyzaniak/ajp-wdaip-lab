5e4a9be0e04b34786333255160f8a9eae6eae404
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-30 21:05:53 +0100

    Zadanie 9.5

---
+++ Zadanie 9/Zadanie 9.5/Zadanie 9.5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float suma = 0, srednia = 0;
+
+float wczytaj() {
+
+		float x = 0;
+		ocena:
+		cout << "Podaj ocene: ";
+		cin >> x;
+		if (x >= 1 && x <= 6) {
+			return x;
+		}
+		else {
+			cout << "Podaj ocene w zakresie od 1 do 6." << endl;;
+			goto ocena;
+		}
+}
+
+int main()
+{	
+	for (int i = 0; i < 5; i++) {
+
+		suma = suma + wczytaj();
+	}
+	srednia = suma / 5;
+	cout << "Srednia ocen to: " << srednia;
+}
