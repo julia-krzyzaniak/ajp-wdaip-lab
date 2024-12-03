deba1e990727e8e22f26c76a105d250df7e8449c
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-12-03 22:21:01 +0100

    Zadanie 10.1

---
+++ Zadanie 10/Zadanie 10.1/Zadanie 10.1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+float pole(float bok) {
+	return  pow(bok, 2);
+}
+
+float pole(float bok1, float bok2) {
+	return  bok1 * bok2;
+}
+
+int main()
+{
+	int f;
+	float a = 0, b = 0;
+
+
+	do {
+		cout << "Wybierz figure, ktorej chcesz obliczyc pole: " << endl;
+		cout << "1 - Kwadrat" << endl;
+		cout << "2 - Prostokat" << endl;
+		cout << "0 - Koniec programu" << endl;
+		cout << endl << "Wybierz opcje: ";
+		cin >> f;
+
+		if (f == 1) {
+			cout << "Podaj bok a: ";
+			cin >> a;
+			cout << "Pole kwadratu to: " << pole(a) << endl << endl;
+		}
+
+		if (f == 2) {
+			cout << "Podaj bok a: ";
+			cin >> a;
+			cout << "Podaj bok b: ";
+			cin >> b;
+			cout << "Pole kwadratu to: " << pole(a, b) << endl << endl;
+		}
+
+	} while (f != 0);
+
+
+}
+
