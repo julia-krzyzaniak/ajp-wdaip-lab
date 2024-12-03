a9ca00daf03430bf50f7efcd6d1501e7060e9a58
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-12-03 23:13:16 +0100

    Zadanie 7.2

---
+++ Zadanie 7/Zadanie 7.2/Zadanie 7.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	const int n = 30;
+	int tab1[n], tab2[n];
+
+	cout << "Program generujacy tablice 30 elementowa o wartosciach losowych z przedzialu od -10 do 10: \n";
+
+	for (int i = 0; i < n; i++)
+	{
+		tab1[i] = (rand() % 21) - 10;
+		cout << tab1[i] << " ";
+	}
+
+	cout << endl << endl;
+
+	cout << "Odwrocenie tabeli: " << endl;
+
+	for (int i = 0, j = n - 1; i < n; i++, j--)
+	{
+		tab2[i] = tab1[j];
+		cout << tab2[i] << " ";
+	}
+}
