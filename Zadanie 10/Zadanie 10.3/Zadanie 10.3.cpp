bee3654b753d71aa663d27cc3de1f1cf073b3fca
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-12-03 22:51:21 +0100

    Zadanie 10.3

---
+++ Zadanie 10/Zadanie 10.3/Zadanie 10.3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int suma_cyfr(int liczba) {
+	int wynik = 0;
+
+	do {
+		wynik = wynik + liczba % 10;
+		liczba = liczba / 10;
+	} while (liczba != 0);
+	
+	return wynik;
+}
+
+int main()
+{
+	int liczba;
+
+	cout << "Program obliczajacy sume cyfr" << endl << endl;
+	cout << "Podaj cyfry: ";
+	cin >> liczba;
+	cout << "Suma cyfr to: " << suma_cyfr(liczba);
+   
+}
+
+
