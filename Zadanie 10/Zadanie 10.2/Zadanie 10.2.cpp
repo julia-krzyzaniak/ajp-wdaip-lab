b9d74cc2bbbe9fbbbcecaf4696601f03006729a7
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-12-03 22:37:17 +0100

    Zadanie 10.2

---
+++ Zadanie 10/Zadanie 10.2/Zadanie 10.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int fibo(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main()
+{
+    int n;
+    cout << "Program obliczajacy n-ty wyraz ciagu Fibonacciego: " << endl << endl;
+    cout << "Podaj liczbe: ";
+    cin >> n;
+    cout << n << "-ty wyraz ciagu Fibonacciego to: " << fibo(n);
+}
+
